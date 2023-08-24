3eea140f59918c1894cd6d102a59a00ed6d5c860
manurrell <borrellmanuel1@gmail.com>
2023-08-23 23:37:23 -0300

    aa

--- routers/apiExterna.js
+++ routers/apiExterna.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const apiExterna = require('../controllers/apiexternacontroller');
 
-router.get('/pokemon/:name', apiExterna);
+router.get('/pokemon/:name', apiExterna.BuscarPokemon);
 
 module.exports = router;
